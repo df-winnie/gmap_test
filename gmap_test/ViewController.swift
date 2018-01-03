f83782132cb8f747da8c82eee59f5051241ac638
df-dev <df-dev@df-devde-MacBook-Pro.local>
2018-01-03 17:39:30 +0800

    1. distance matrix 2. popup view

--- gmap_test/ViewController.swift
+++ gmap_test/ViewController.swift
@@ -22,7 +22,148 @@ class ViewController: UIViewController {
     let kCameraLongitude = 120.6839
     
     var popupController:CNPPopupController?
+    
+    private var currentLocation:CLLocation!
+    
+    //MARK: Starter Example
+    private func setLocation() {
+        
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: 24.1469, longitude: 120.6839, zoom: 15.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.isMyLocationEnabled = true
+        mapView.accessibilityElementsHidden = false
+        
+        view = mapView
+        
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: 24.1469, longitude: 120.6839)
+        marker.title = "Taichung"
+        marker.snippet = "Taiwan"
+        marker.map = mapView
+    }
+    
+    private func calcDistance(destLocation:CLLocation) {
 
+        let clat = String(describing: currentLocation.coordinate.latitude)
+        let clng = String(describing: currentLocation.coordinate.longitude)
+        
+        let dlat = String(describing: destLocation.coordinate.latitude)
+        let dlng = String(describing: destLocation.coordinate.longitude)
+        
+        let url = URL(string: "https://maps.googleapis.com/maps/api/distancematrix/json?language=zh-TW&origins=\(clat),\(clng)&destinations=\(dlat),\(dlng)&key=AIzaSyCsdQewjR0Om5JkvQSQiUMaRnrryNT_Fh0") // units=metric
+        
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if let data = data {
+                do {
+                    // Convert the data to JSON
+                    let jsonSerialized = try JSONSerialization.jsonObject(with: data, options: []) as? [String : Any]
+                    
+                    if let json = jsonSerialized,
+                        let status = json["status"] as? String,
+                        let rows = json["rows"] as? [Any],
+                        let originAdds = json["origin_addresses"] as? [String],
+                        let destinationAdds = json["destination_addresses"] as? [String] {
+                        
+                        if status == "OK" {
+//                            guard let rows = jrows as? [Any],
+                            guard let firstrow = rows.first as? [String:Any],
+                                let elements = firstrow["elements"] as? [Any],
+                                let item = elements.first as? [String:Any],
+                                let distance = item["distance"] as? [String:Any],
+                                let duration = item["duration"] as? [String:Any] else {
+                                    print("dafaq?")
+                                    return
+                            }
+                            
+                            guard let dAdd = destinationAdds.first,
+                                let dist = distance["text"] as? String,
+                                let dur = duration["text"] as? String else {
+                                    print("dafaq??")
+                                    return
+                            }
+                            
+                            DispatchQueue.main.async {
+                                self.showPopup(dAdd: dAdd, dist: dist, dur: dur)
+                            }
+                        }
+                    }
+                }  catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
+    @objc func showPopup(dAdd:String,dist:String,dur:String) {
+        
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineBreakMode = NSLineBreakMode.byWordWrapping
+        paragraphStyle.alignment = NSTextAlignment.center
+        
+        let title = NSAttributedString(string: "\(dAdd)", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 24), NSAttributedStringKey.paragraphStyle: paragraphStyle])
+        // It's A Popup!
+        let lineOne = NSAttributedString(string: "距離：\(dist)\n時間：\(dur)", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 18), NSAttributedStringKey.paragraphStyle: paragraphStyle])
+        // You can add text and images
+        //        let lineTwo = NSAttributedString(string: "With style, using NSAttributedString", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18), NSAttributedStringKey.foregroundColor: UIColor.init(colorLiteralRed: 0.46, green: 0.8, blue: 1.0, alpha: 1.0), NSParagraphStyleAttributeName: paragraphStyle])
+        
+        let button = CNPPopupButton.init(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
+        button.setTitleColor(UIColor.white, for: UIControlState())
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.setTitle("Close Me", for: UIControlState())
+        
+        button.backgroundColor = .green
+        
+        button.layer.cornerRadius = 4;
+        button.selectionHandler = { (button) -> Void in
+            self.popupController?.dismiss(animated: true)
+            print("Block for button: \(button.titleLabel?.text)")
+        }
+        
+        let titleLabel = UILabel()
+        titleLabel.numberOfLines = 0;
+        titleLabel.attributedText = title
+        
+        let lineOneLabel = UILabel()
+        lineOneLabel.numberOfLines = 0;
+        lineOneLabel.attributedText = lineOne;
+        lineOneLabel.backgroundColor = .yellow
+        
+        let imageView = UIImageView.init(image: UIImage.init(named: "icon"))
+        
+        let lineTwoLabel = UILabel()
+        lineTwoLabel.numberOfLines = 0;
+        //        lineTwoLabel.attributedText = lineTwo;
+        
+        let customView = UIView.init(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
+        customView.backgroundColor = UIColor.lightGray
+        
+        let textField = UITextField.init(frame: CGRect(x: 10, y: 10, width: 230, height: 35))
+        textField.borderStyle = UITextBorderStyle.roundedRect
+        textField.placeholder = "Custom view!"
+        customView.addSubview(textField)
+        
+        let popupController = CNPPopupController(contents:[titleLabel, lineOneLabel, imageView, lineTwoLabel, customView, button])
+        popupController.theme = CNPPopupTheme.default()
+        popupController.theme.popupStyle = .actionSheet
+        // LFL added settings for custom color and blur
+        popupController.theme.maskType = .dimmed
+        //        popupController.theme.customMaskColor = UIColor.red
+        //        popupController.theme.blurEffectAlpha = 1.0
+        popupController.delegate = self
+        self.popupController = popupController
+        popupController.present(animated: true)
+        
+    }
     
     // MARK: - Clustering
     func clustering(sites:[bikeSite]) {
@@ -51,6 +192,7 @@ class ViewController: UIViewController {
     /// Randomly generates cluster items within some extent of the camera and
     /// adds them to the cluster manager.
     private func generateClusterItems(sites:[bikeSite]) {
+        
 //        let extent = 0.2
 //        for index in 1...kClusterItemCount {
         for point in sites {
@@ -90,86 +232,9 @@ class ViewController: UIViewController {
         marker.map = mapView
     }
     
-    // starter example
-    private func setLocation() {
-        
-        // Create a GMSCameraPosition that tells the map to display the
-        // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: 24.1469, longitude: 120.6839, zoom: 15.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        mapView.isMyLocationEnabled = true
-        mapView.accessibilityElementsHidden = false
-
-        view = mapView
-        
-        // Creates a marker in the center of the map.
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: 24.1469, longitude: 120.6839)
-        marker.title = "Taichung"
-        marker.snippet = "Taiwan"
-        marker.map = mapView
-    }
-    
-    @objc func showPopup(sender:UIButton?) {
-        
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.lineBreakMode = NSLineBreakMode.byWordWrapping
-        paragraphStyle.alignment = NSTextAlignment.center
-        
-        let title = NSAttributedString(string: "It's A Popup!", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 24), NSAttributedStringKey.paragraphStyle: paragraphStyle])
-        let lineOne = NSAttributedString(string: "You can add text and images", attributes: [NSAttributedStringKey.font: UIFont.systemFont(ofSize: 18), NSAttributedStringKey.paragraphStyle: paragraphStyle])
-//        let lineTwo = NSAttributedString(string: "With style, using NSAttributedString", attributes: [NSFontAttributeName: UIFont.systemFont(ofSize: 18), NSAttributedStringKey.foregroundColor: UIColor.init(colorLiteralRed: 0.46, green: 0.8, blue: 1.0, alpha: 1.0), NSParagraphStyleAttributeName: paragraphStyle])
-        
-        let button = CNPPopupButton.init(frame: CGRect(x: 0, y: 0, width: 200, height: 60))
-        button.setTitleColor(UIColor.white, for: UIControlState())
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
-        button.setTitle("Close Me", for: UIControlState())
-        
-        button.backgroundColor = .green
-        
-        button.layer.cornerRadius = 4;
-        button.selectionHandler = { (button) -> Void in
-            self.popupController?.dismiss(animated: true)
-            print("Block for button: \(button.titleLabel?.text)")
-        }
-        
-        let titleLabel = UILabel()
-        titleLabel.numberOfLines = 0;
-        titleLabel.attributedText = title
-        
-        let lineOneLabel = UILabel()
-        lineOneLabel.numberOfLines = 0;
-        lineOneLabel.attributedText = lineOne;
-        
-        let imageView = UIImageView.init(image: UIImage.init(named: "icon"))
-        
-        let lineTwoLabel = UILabel()
-        lineTwoLabel.numberOfLines = 0;
-//        lineTwoLabel.attributedText = lineTwo;
-        
-        let customView = UIView.init(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
-        customView.backgroundColor = UIColor.lightGray
-        
-        let textField = UITextField.init(frame: CGRect(x: 10, y: 10, width: 230, height: 35))
-        textField.borderStyle = UITextBorderStyle.roundedRect
-        textField.placeholder = "Custom view!"
-        customView.addSubview(textField)
-        
-        let popupController = CNPPopupController(contents:[titleLabel, lineOneLabel, imageView, lineTwoLabel, customView, button])
-        popupController.theme = CNPPopupTheme.default()
-        popupController.theme.popupStyle = .actionSheet
-        // LFL added settings for custom color and blur
-        popupController.theme.maskType = .dimmed
-//        popupController.theme.customMaskColor = UIColor.red
-//        popupController.theme.blurEffectAlpha = 1.0
-        popupController.delegate = self
-        self.popupController = popupController
-        popupController.present(animated: true)
-        
-    }
 }
 
-
+//MARK:Life Cycle
 extension ViewController {
     
     override func viewDidLoad() {
@@ -184,7 +249,6 @@ extension ViewController {
         
         view.addSubview(btn)
         */
-        
 
         // your map initiation code
         mapView = GMSMapView(frame: self.view.frame)
@@ -199,10 +263,7 @@ extension ViewController {
         locationManager.startUpdatingLocation()
         
         view.addSubview(mapView)
-        
-//        setLocation()
-//        clustering()
-        
+ 
         WebService.bikeSites(urlStr: "http://data.taichung.gov.tw/wSite/public/data/f1501559780355.json") { (list, error) in
 
             DispatchQueue.main.async {
@@ -224,20 +285,26 @@ extension ViewController {
     }
 }
 
+//MARK:CLLocationManagerDelegate
 extension ViewController:CLLocationManagerDelegate {
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        let location = locations.last
-        print(location?.coordinate.latitude, location?.coordinate.longitude, "CURRENT LOCATION")
-        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude: (location?.coordinate.longitude)!, zoom: 17.0)
+        if let location = locations.last {
+            self.currentLocation = location
+            
+            print(location.coordinate.latitude, location.coordinate.longitude, "CURRENT LOCATION")
+            let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude, longitude: location.coordinate.longitude, zoom: 17.0)
+            
+            self.mapView.animate(to: camera)
+            
+            //Finally stop updating location otherwise it will come again and again in this delegate
+            self.locationManager.stopUpdatingLocation()
+        }
         
-        self.mapView.animate(to: camera)
-        
-        //Finally stop updating location otherwise it will come again and again in this delegate
-        self.locationManager.stopUpdatingLocation()
     }
 }
 
+//MARK:GMUClusterManagerDelegate
 extension ViewController:GMUClusterManagerDelegate {
     
     func clusterManager(clusterManager: GMUClusterManager, didTapCluster cluster: GMUCluster) {
@@ -248,7 +315,7 @@ extension ViewController:GMUClusterManagerDelegate {
     }
 }
 
-
+//MARK:GMSMapViewDelegate
 extension ViewController:GMSMapViewDelegate {
     
     func mapView(_ mapView: GMSMapView, didTap marker: GMSMarker) -> Bool {
@@ -256,8 +323,9 @@ extension ViewController:GMSMapViewDelegate {
         if let poiItem = marker.userData as? POIItem {
             // tap on marker
             NSLog("Did tap marker for cluster item \(poiItem.name)")
-            print(poiItem.position,"TAPPED LOCATION")
-            self.showPopup(sender: nil)
+            print("TAPPED LOCATION",poiItem.position)
+//            self.showPopup(destLocation: CLLocation(latitude: poiItem.position.latitude, longitude: poiItem.position.longitude))
+            self.calcDistance(destLocation: CLLocation(latitude: poiItem.position.latitude, longitude: poiItem.position.longitude))
         } else {
             // tap on cluster marker
             NSLog("Did tap a normal marker")
@@ -270,6 +338,7 @@ extension ViewController:GMSMapViewDelegate {
     
 }
 
+//MARK:CNPPopupControllerDelegate
 extension ViewController : CNPPopupControllerDelegate {
     
     func popupControllerWillDismiss(_ controller: CNPPopupController) {
